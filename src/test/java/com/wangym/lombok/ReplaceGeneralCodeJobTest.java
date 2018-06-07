85fc767e960b382eb14def4e3e3507e91ba9cf7f
wangym <wangyuming11725@gmail.com>
2018-06-07 14:26:30 +0800

    添加测试用例

---
+++ src/test/java/com/wangym/lombok/ReplaceGeneralCodeJobTest.java
@@ -0,0 +1,46 @@
+package com.wangym.lombok;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+import com.wangym.lombok.job.ReplaceGeneralCodeJob;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author wangym
+ * @version 创建时间：2018年6月7日 上午9:56:09
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReplaceGeneralCodeJobTest {
+
+    @Autowired
+    private ReplaceGeneralCodeJob job;
+
+    @Test
+    public void handle() throws IOException {
+        job.handle(new File("c:/AppResult.java"));
+    }
+
+    @Test
+    public void print() throws IOException {
+        System.setProperty("line.separator", "\n");
+        CompilationUnit compilationUnit = JavaParser.parse("package com.wangym.test;\nclass A{ }");
+        LexicalPreservingPrinter.setup(compilationUnit);
+        NodeList<ImportDeclaration> imports = compilationUnit.getImports();
+        String str = "lombok.Data";
+        imports.add(new ImportDeclaration(str, false, false));
+        String newBody = LexicalPreservingPrinter.print(compilationUnit);
+        FileCopyUtils.copy(newBody.getBytes("utf-8"), new File("c:/A.java"));
+    }
+}
