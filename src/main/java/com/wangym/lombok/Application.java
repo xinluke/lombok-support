9d3acacd30ed34f28adb628541a96b619b2f864a
wangym <wangyuming11725@gmail.com>
2018-06-28 13:56:47 +0800

    抽取逻辑至controller中

--- src/main/java/com/wangym/lombok/Application.java
+++ src/main/java/com/wangym/lombok/Application.java
@@ -1,13 +1,9 @@
 package com.wangym.lombok;
 
-import com.wangym.lombok.job.ReplaceGeneralCodeJob;
-import com.wangym.lombok.job.ReplaceLoggerJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.IOException;
 
@@ -17,25 +13,9 @@ public class Application {
 
     public static void main(String[] args) throws IOException {
         ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-        ReplaceLoggerJob modifyJob = ctx.getBean(ReplaceLoggerJob.class);
-        ReplaceGeneralCodeJob codeJob = ctx.getBean(ReplaceGeneralCodeJob.class);
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         // 设置javaparse输出文本的换行符
         System.setProperty("line.separator", "\n");
-        // 以当前jar为基点，搜索下面全部的文件
-        Resource[] resources = resolver.getResources("file:./**");
-        for (Resource resource : resources) {
-            if (!resource.getFilename().endsWith(".java")) {
-                continue;
-            }
-            String uriStr = resource.getURI().toString();
-            log.debug("尝试读取文件：{}", uriStr);
-            try {
-                modifyJob.handle(resource.getFile());
-                codeJob.handle(resource.getFile());
-            } catch (Exception e) {
-                log.info("读取文件失败：{}", uriStr, e);
-            }
-        }
+        JobController ctrl = ctx.getBean(JobController.class);
+        ctrl.start();
     }
 }
---
+++ src/main/java/com/wangym/lombok/JobController.java
@@ -0,0 +1,48 @@
+package com.wangym.lombok;
+
+import com.wangym.lombok.job.Job;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author wangym
+ * @version 创建时间：2018年6月28日 下午1:32:48
+ */
+@Service
+@Slf4j
+public class JobController {
+
+    @Autowired
+    private List<Job> jobList;
+
+    public void start() {
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        // 以当前jar为基点，搜索下面全部的文件
+        try {
+            Resource[] resources = resolver.getResources("file:./**");
+            for (Resource resource : resources) {
+                diapatch(resource);
+            }
+        } catch (IOException e) {
+            log.info("未执行完成", e);
+        }
+    }
+
+    private void diapatch(Resource resource) {
+        for (Job job : jobList) {
+            if (job.canRead(resource.getFilename())) {
+                try {
+                    job.handle(resource.getFile());
+                } catch (IOException e) {
+                    log.info("读取文件失败", e);
+                }
+            }
+        }
+    }
+}
