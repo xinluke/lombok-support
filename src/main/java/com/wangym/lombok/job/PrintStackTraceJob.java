529a8303cdc807afb8beb59da29a49f3b9e3d60d
wangym <wangyuming11725@gmail.com>
2018-12-28 19:58:32 +0800

    printStackTrace的Job代码优化

--- src/main/java/com/wangym/lombok/job/PrintStackTraceJob.java
+++ src/main/java/com/wangym/lombok/job/PrintStackTraceJob.java
@@ -2,20 +2,19 @@ package com.wangym.lombok.job;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.StringLiteralExpr;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
 import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author wangym
@@ -25,67 +24,53 @@ import java.util.stream.Collectors;
 @Slf4j
 public class PrintStackTraceJob extends JavaJob {
 
+    private Metadata meta = new Metadata("Slf4j", "lombok.extern.slf4j.Slf4j");
+
     @Override
     public void handle(File file) throws IOException {
         byte[] bytes = FileCopyUtils.copyToByteArray(file);
         CompilationUnit compilationUnit = JavaParser.parse(new String(bytes, "utf-8"));
-        List<ClassMehodCallMapping> methodCallList = extractAllMethodCallExpr(compilationUnit);
-        if (methodCallList.isEmpty()) {
-            return;
+        PrintStackTraceVisitor visitor = new PrintStackTraceVisitor();
+        compilationUnit.clone().accept(visitor, null);
+        if (visitor.isModify()) {
+            log.info("存在[e.printStackTrace()]的代码块，将进行替换");
+            LexicalPreservingPrinter.setup(compilationUnit);
+            compilationUnit.accept(visitor, null);
+            addImports(compilationUnit, meta);
+            String newBody = LexicalPreservingPrinter.print(compilationUnit);
+            // 以utf-8编码的方式写入文件中
+            FileCopyUtils.copy(newBody.toString().getBytes("utf-8"), file);
         }
-        log.info("存在[e.printStackTrace()]的代码块，将进行替换");
-        LexicalPreservingPrinter.setup(compilationUnit);
-        Metadata meta = new Metadata("Slf4j", "lombok.extern.slf4j.Slf4j");
-        methodCallList.forEach(it -> process(it.getExprs()));
-        methodCallList.forEach(it -> addAnnotation(it.getC(), meta));
-        addImports(compilationUnit, meta);
-        String newBody = LexicalPreservingPrinter.print(compilationUnit);
-        // 以utf-8编码的方式写入文件中
-        FileCopyUtils.copy(newBody.toString().getBytes("utf-8"), file);
     }
 
-    private List<ClassMehodCallMapping> extractAllMethodCallExpr(CompilationUnit compilationUnit) {
-        List<ClassOrInterfaceDeclaration> classList = compilationUnit.findAll(ClassOrInterfaceDeclaration.class);
-        List<ClassMehodCallMapping> result = new ArrayList<>();
-        for (ClassOrInterfaceDeclaration c : classList) {
-            // 不是顶级类就跳过，因为会递归查出这个类下面关联的全部方法调用代码块
-            if (!c.isTopLevelType()) {
-                continue;
+    @Getter
+    class PrintStackTraceVisitor extends ModifierVisitor<Void> {
+        private boolean modify = false;
+
+        @Override
+        public Visitable visit(MethodCallExpr it, Void arg) {
+            if ("printStackTrace".equals(it.getName().toString()) && it.getArguments().isEmpty()) {
+                // 设置标志位
+                modify = true;
+                return process(it);
             }
-            result.add(new ClassMehodCallMapping(c) {
-
-                @Override
-                boolean filter(MethodCallExpr it) {
-                    if ("printStackTrace".equals(it.getName().toString()) && it.getArguments().isEmpty()) {
-                        return true;
-                    }
-                    return false;
-                }
-
-            });
+            return super.visit(it, arg);
         }
-        return result.stream()
-                .filter(item -> {
-                    return !item.getExprs().isEmpty();
-                })
-                .collect(Collectors.toList());
-    }
 
-    private void process(List<MethodCallExpr> exprs) {
-        exprs.forEach(this::process);
-    }
+        private Visitable process(MethodCallExpr expr) {
+            /*
+             * 将e.printStackTrace();
+             * 替换成log.error("unexpected exception,please check",e);
+             */
+            String var = expr.getScope().get().toString();
+            String str = "log";
+            expr.setScope(new NameExpr(str));
+            expr.setName("error");
+            expr.getArguments().add(new StringLiteralExpr("unexpected exception,please check"));
+            expr.getArguments().add(new NameExpr(var));
+            return expr;
+        }
 
-    private void process(MethodCallExpr expr) {
-        /*
-         * 将e.printStackTrace();
-         * 替换成log.error("unexpected exception,please check",e);
-         */
-        String var = expr.getScope().get().toString();
-        String str = "log";
-        expr.setScope(new NameExpr(str));
-        expr.setName("error");
-        expr.getArguments().add(new StringLiteralExpr("unexpected exception,please check"));
-        expr.getArguments().add(new NameExpr(var));
     }
 
 }
