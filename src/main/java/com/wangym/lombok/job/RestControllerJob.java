959276b6c8750218481451bf57d7a7a3adecd71f
wangym <wangyuming11725@gmail.com>
2019-01-07 14:12:00 +0800

    优化RestControllerJob

--- src/main/java/com/wangym/lombok/job/RestControllerJob.java
+++ src/main/java/com/wangym/lombok/job/RestControllerJob.java
@@ -6,78 +6,95 @@ import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
 import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author wangym
  * @version 创建时间：2018年8月2日 下午8:31:53
  */
 @Component
+@Slf4j
 public class RestControllerJob extends JavaJob {
 
     @Override
     public void handle(File file) throws IOException {
         byte[] bytes = FileCopyUtils.copyToByteArray(file);
         CompilationUnit compilationUnit = JavaParser.parse(new String(bytes, "utf-8"));
-        List<String> annNames = Arrays.asList("RestController");
-        List<ClassOrInterfaceDeclaration> cList = compilationUnit.findAll(ClassOrInterfaceDeclaration.class).stream()
-                .filter(it -> {
-                    NodeList<AnnotationExpr> anns = it.getAnnotations();
-                    for (AnnotationExpr item : anns) {
-                        if (annNames.contains(item.getNameAsString())) {
-                            return true;
-                        }
-                    }
-                    return false;
-                })
-                .collect(Collectors.toList());
-        List<AnnotationExpr> annList = new ArrayList<>();
-        for (ClassOrInterfaceDeclaration c : cList) {
-            for (MethodDeclaration method : c.getMethods()) {
-                NodeList<AnnotationExpr> anns = method.getAnnotations();
-                AnnotationExpr result = test(anns);
-                if (result != null) {
-                    annList.add(result);
+        ResponseBodyConstructorVisitor visitor = new ResponseBodyConstructorVisitor();
+        compilationUnit.clone().accept(visitor, null);
+        if (visitor.isModify()) {
+            log.info("存在[@ResponseBody]重复代码块，将进行删除");
+            LexicalPreservingPrinter.setup(compilationUnit);
+            compilationUnit.accept(visitor, null);
+            String newBody = LexicalPreservingPrinter.print(compilationUnit);
+            // 以utf-8编码的方式写入文件中
+            FileCopyUtils.copy(newBody.toString().getBytes("utf-8"), file);
+        }
+    }
+
+    @Getter
+    class ResponseBodyConstructorVisitor extends ModifierVisitor<Void> {
+        private boolean modify = false;
+
+        @Override
+        public Visitable visit(ClassOrInterfaceDeclaration n, Void arg) {
+            if (isTarget(n)) {
+                for (MethodDeclaration method : n.getMethods()) {
+                    NodeList<AnnotationExpr> anns = method.getAnnotations();
+                    test(anns);
+                }
+                return n;
+            }
+            return super.visit(n, arg);
+        }
+
+        private boolean isTarget(ClassOrInterfaceDeclaration it) {
+            List<String> annNames = Arrays.asList("RestController");
+            NodeList<AnnotationExpr> anns = it.getAnnotations();
+            for (AnnotationExpr item : anns) {
+                if (annNames.contains(item.getNameAsString())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void test(NodeList<AnnotationExpr> list) {
+            List<String> mappingMethod = Arrays.asList("RequestMapping", "GetMapping", "PostMapping", "PutMapping",
+                    "DeleteMapping", "PatchMapping");
+            boolean isMappingMethod = list.stream()
+                    .filter(it -> mappingMethod.contains(it.getNameAsString()))
+                    .count() == 1;
+            if (isMappingMethod) {
+                AnnotationExpr expr = getUnusedAnnotationExpr(list);
+                if (expr != null) {
+                    expr.remove();
+                    // 设置标志位
+                    modify = true;
                 }
             }
         }
-        if (annList.isEmpty()) {
-            return;
-        }
-        LexicalPreservingPrinter.setup(compilationUnit);
-        annList.forEach(it -> it.remove());
-        String newBody = LexicalPreservingPrinter.print(compilationUnit);
-        // 以utf-8编码的方式写入文件中
-        FileCopyUtils.copy(newBody.toString().getBytes("utf-8"), file);
-    }
 
-    private AnnotationExpr test(NodeList<AnnotationExpr> list) {
-        List<String> mappingMethod = Arrays.asList("RequestMapping", "GetMapping", "PostMapping", "PutMapping",
-                "DeleteMapping", "PatchMapping");
-        boolean isMappingMethod = list.stream()
-                .filter(it -> mappingMethod.contains(it.getNameAsString()))
-                .count() == 1;
-        if (isMappingMethod) {
-            return getUnusedAnnotationExpr(list);
-        }
-        return null;
-    }
-
-    private AnnotationExpr getUnusedAnnotationExpr(NodeList<AnnotationExpr> list) {
-        for (AnnotationExpr it : list) {
-            if ("ResponseBody".equals(it.getNameAsString())) {
-                return it;
+        private AnnotationExpr getUnusedAnnotationExpr(NodeList<AnnotationExpr> list) {
+            for (AnnotationExpr it : list) {
+                if ("ResponseBody".equals(it.getNameAsString())) {
+                    return it;
+                }
             }
+            return null;
         }
-        return null;
+
     }
+
 }
