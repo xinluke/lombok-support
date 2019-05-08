4875d28504a8d8b1c5e2f4916b7fc16b985536b5
wangym <wangyuming11725@gmail.com>
2019-05-08 11:11:28 +0800

    添加抽取接口的业务

---
+++ src/main/java/com/wangym/lombok/job/impl/ExtractJob.java
@@ -0,0 +1,61 @@
+package com.wangym.lombok.job.impl;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+import com.wangym.lombok.job.JavaJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+@Component
+@ConditionalOnProperty(value = "extract.enable", havingValue = "true")
+@Slf4j
+public class ExtractJob extends JavaJob {
+
+    @Override
+    public void handle(File file) throws IOException {
+        byte[] bytes = FileCopyUtils.copyToByteArray(file);
+        CompilationUnit compilationUnit = JavaParser.parse(new String(bytes, "utf-8"));
+        ExtractVisitor visitor = new ExtractVisitor();
+        LexicalPreservingPrinter.setup(compilationUnit);
+        compilationUnit.accept(visitor, null);
+        String newBody = LexicalPreservingPrinter.print(compilationUnit);
+        // 以utf-8编码的方式写入文件中
+        FileCopyUtils.copy(newBody.toString().getBytes("utf-8"), file);
+    }
+
+    class ExtractVisitor extends ModifierVisitor<Void> {
+
+        @Override
+        public Visitable visit(BlockStmt n, Void arg) {
+            // 不要方法体
+            return null;
+        }
+
+        @Override
+        public Visitable visit(ClassOrInterfaceDeclaration n, Void arg) {
+            // 换成接口
+            if (!n.isInterface()) {
+                n.setInterface(true);
+            }
+            return super.visit(n, arg);
+        }
+
+        @Override
+        public Visitable visit(FieldDeclaration n, Void arg) {
+            // 不要字段声明
+            return null;
+        }
+
+    }
+}
---
+++ src/test/java/com/wangym/lombok/ExtractJobTest.java
@@ -0,0 +1,27 @@
+package com.wangym.lombok;
+
+import com.wangym.lombok.job.impl.ExtractJob;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author wangym
+ * @version 创建时间：2018年11月6日 下午2:48:11
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExtractJobTest {
+    @Autowired
+    private ExtractJob job;
+
+    @Test
+    public void handle() throws IOException {
+        job.handle(new File("c:/TrackingController.java"));
+    }
+}
