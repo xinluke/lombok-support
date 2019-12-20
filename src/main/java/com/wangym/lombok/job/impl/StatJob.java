0f9e528be2c9c6ffe6b3f35d2b970b936d17095f
wangym <wangyuming11725@gmail.com>
2019-12-20 20:57:33 +0800

    添加统计任务

---
+++ src/main/java/com/wangym/lombok/job/impl/StatJob.java
@@ -0,0 +1,89 @@
+package com.wangym.lombok.job.impl;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
+import com.wangym.lombok.job.JavaJob;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+
+import javax.annotation.PreDestroy;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author wangym
+ * @version 创建时间：2018年11月6日 下午2:27:46
+ */
+@Component
+@Slf4j
+public class StatJob extends JavaJob {
+    // 单词和数量映射表
+    HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+
+    @Override
+    public void handle(File file) throws IOException {
+        byte[] bytes = FileCopyUtils.copyToByteArray(file);
+        CompilationUnit compilationUnit = JavaParser.parse(new String(bytes, "utf-8"));
+        StatVisitor visitor = new StatVisitor();
+        compilationUnit.clone().accept(visitor, null);
+    }
+
+    @PreDestroy
+    public void print() {
+        Iterator<String> iterator = hashMap.keySet().iterator();
+        while (iterator.hasNext()) {
+            String word = iterator.next();
+
+//          System.out.printf("单词： "+word+"出现次数："+hashMap.get(word));
+            System.out.printf("单词:%-12s 出现次数:%d\n", word, hashMap.get(word));
+        }
+    }
+
+    @Getter
+    class StatVisitor extends ModifierVisitor<Void> {
+
+        @Override
+        public Visitable visit(ClassOrInterfaceDeclaration n, Void arg) {
+            // 类名或者接口名
+            String nameAsString = n.getNameAsString();
+
+            process(nameAsString);
+            return super.visit(n, arg);
+        }
+
+        private void process(String word) {
+            Set<String> wordSet = hashMap.keySet();
+            // 如果已经有这个单词了，
+            if (wordSet.contains(word)) {
+                Integer number = hashMap.get(word);
+                number++;
+                hashMap.put(word, number);
+            } else {
+                hashMap.put(word, 1);
+            }
+        }
+
+//        @Override
+//        public Visitable visit(VariableDeclarator n, Void arg) {
+//            System.out.println(n.getNameAsString());
+//            return super.visit(n, arg);
+//        }
+        @Override
+        public Visitable visit(MethodDeclaration n, Void arg) {
+            String nameAsString = n.getNameAsString();
+            process(nameAsString);
+            return super.visit(n, arg);
+        }
+    }
+
+}
