f24ff53cac8cdf478a26cfbe7cd87bb8030ddef9
wangym <wangyuming11725@gmail.com>
2022-07-15 20:41:08 +0800

    添加共用方法

--- src/main/java/com/wangym/lombok/job/AbstractJob.java
+++ src/main/java/com/wangym/lombok/job/AbstractJob.java
@@ -7,6 +7,10 @@ import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.expr.AnnotationExpr;
 import com.github.javaparser.ast.expr.MarkerAnnotationExpr;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author wangym
  * @version 创建时间：2018年8月4日 下午5:33:47
@@ -32,6 +36,49 @@ public abstract class AbstractJob implements Job {
         if (notExist) {
             anns.add(new MarkerAnnotationExpr(name));
         }
+    }
+
+    protected void deleteImports(CompilationUnit compilationUnit, Metadata meta) {
+        NodeList<ImportDeclaration> imports = compilationUnit.getImports();
+        String str = meta.getImportPkg();
+        boolean exist = imports.stream()
+                .filter(it -> {
+                    String asString = it.getName().asString();
+                    // 普通模式的比较
+                    return str.equals(asString);
+                })
+                .count() > 0;
+        if (exist) {
+            imports.remove(new ImportDeclaration(str, false, false));
+        }
+    }
+
+    protected void deleteImports(CompilationUnit compilationUnit, List<String> deleteImports) {
+        NodeList<ImportDeclaration> imports = compilationUnit.getImports();
+        imports.stream()
+                .filter(it -> {
+                    return deleteImports.contains(it.getName().asString());
+                })
+                // 不可边循环边删除,所以先filter出一个集合再删除
+                .collect(Collectors.toList())
+                .forEach(it -> compilationUnit.remove(it));
+    }
+
+    protected void replaceImportsIfExist(CompilationUnit compilationUnit, Metadata oldMeta, Metadata newMeta) {
+        List<String> deleteImports= Arrays.asList(oldMeta.getImportPkg());
+        NodeList<ImportDeclaration> imports = compilationUnit.getImports();
+        List<ImportDeclaration> res = imports.stream()
+                .filter(it -> {
+                    return deleteImports.contains(it.getName().asString());
+                })
+                // 不可边循环边删除,所以先filter出一个集合再删除
+                .collect(Collectors.toList());
+        if (!res.isEmpty()) {
+            res.stream()
+                    .forEach(it -> compilationUnit.remove(it));
+            addImports(compilationUnit, newMeta);
+        }
+
     }
 
     protected void addImports(CompilationUnit compilationUnit, Metadata meta) {
