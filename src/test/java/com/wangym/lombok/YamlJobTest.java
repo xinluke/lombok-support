4635b16b3c9dceb8725d45b7b0dbab6156b48d9e
wangym <wangyuming11725@gmail.com>
2022-07-25 22:14:06 +0800

    使用底层api来处理event进行序列化

--- src/test/java/com/wangym/lombok/YamlJobTest.java
+++ src/test/java/com/wangym/lombok/YamlJobTest.java
@@ -9,10 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.emitter.Emitter;
 import org.yaml.snakeyaml.events.Event;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,11 +38,18 @@ public class YamlJobTest {
             data.add(event);
         }
         System.out.println("YamlJobTest.handle");
+        //使用底层api来处理event
         DumperOptions dopts = new DumperOptions();
         dopts.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        dopts.setProcessComments(true);
         dopts.setIndent(2);
-        Yaml yaml2 = new Yaml(dopts);
-        String text = yaml2.dump(data);
+        StringWriter output = new StringWriter();
+        Emitter emitter = new Emitter(output, dopts);
+        for (Event event : data) {
+            emitter.emit(event);
+        }
+        String text = output.toString();
+
         System.out.println("text = " + text);
 
     }
