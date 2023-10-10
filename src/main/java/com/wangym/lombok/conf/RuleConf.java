3476d428db3a9db06dd42512bcdf83bd55a7f800
wangym <wangyuming11725@gmail.com>
2023-10-10 14:46:12 +0800

    --taskid=ID20231010 设置conf，用来明确是否启用一些特性

--- src/main/java/com/wangym/lombok/conf/RuleConf.java
+++ src/main/java/com/wangym/lombok/conf/RuleConf.java
@@ -2,9 +2,11 @@ package com.wangym.lombok.conf;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Data
 @ConfigurationProperties(prefix = "rule")
+@Configuration
 public class RuleConf {
     /**
      * 是否开启feign name声明配置
